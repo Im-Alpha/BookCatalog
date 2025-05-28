2dfa7d0a4ae2b350192534c925cf02a56144cd97
Jacob Delgado <arcjcb11@gmail.com>
2025-05-27 22:27:57 -0700

    Phase 1.3 complete, connected to DB via dbcontext and npgsql.

--- BookCatalog.Api/Program.cs
+++ BookCatalog.Api/Program.cs
@@ -1,6 +1,27 @@
+using BookCatalog.Api.Data;
+
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Connection String for DB PostgreSQL -- Start --
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+if (string.IsNullOrEmpty(connectionString))
+{
+    // Let the user know what error is coming up
+    throw new InvalidOperationException("Connection string 'DefaultConnection' not found");
+}
+
+builder.Services.AddDbContext<BookCatalogDbContext>(options =>
+{
+    // Use Npgsql provider for PostgreSQL
+    options.UseNpgsql(connectionString);
+});
+
+// DB Connection -- End --
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
